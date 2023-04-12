73f4e21f898ae2cc905f3849ad4240e7dba179b2
phamvietnguyenduy <78025834+phamvietnguyenduy@users.noreply.github.com>
2023-04-12 17:03:11 +0700

    use form

--- client/src/components/pages/Login/Form.tsx
+++ client/src/components/pages/Login/Form.tsx
@@ -1,18 +1,33 @@
 import React, { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
-type Inputs = {
-  example: string;
-  exampleRequired: string;
-};
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup
+  .object({
+    email: yup.string().required(),
+    pass: yup
+      .string()
+      .required()
+      .matches(/^\S*$/, "White Spaces are not allowed")
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[.!+@#\$%\^&\*])(?=.{8,})/,
+        "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
+      ),
+    checkbox: yup.boolean(),
+  })
+  .required();
+type FormData = yup.InferType<typeof schema>;
 const Form = () => {
   const [email, setemail] = useState("");
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
-  } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+  } = useForm<FormData>({
+    resolver: yupResolver(schema),
+  });
+  const onSubmit = (data: FormData) => console.log(data);
   return (
     <div>
       {" "}
@@ -25,21 +40,26 @@ const Form = () => {
         <h1 className="text-2xl font-bold text-thCyan">Welcome Back</h1>
         <input
           type="email"
-          id=""
-          defaultValue="youremail@gmail.com"
-          {...register("exampleRequired", { required: true })}
+          id="emailfield"
+          defaultValue=""
+          {...register("email", { required: true })}
           className=" py-3 pl-3 pr-4 w-[17rem] border-[1px] rounded-lg text-xs"
           placeholder="Enter your email"
         />
+        <p>{errors.pass?.message}</p>
         <input
           type="password"
           id=""
           className=" py-3 pl-3 pr-4 w-[17rem] border-[1px] rounded-lg text-xs"
           placeholder="password"
-          {...register("exampleRequired", { required: true })}
+          {...register("pass", { required: true })}
         />
         <div className="flex justify-start items-center gap-x-2 text-xs w-full">
-          <input type="checkbox" className="form-checkbox text-thRed" />
+          <input
+            {...register("checkbox")}
+            type="checkbox"
+            className="form-checkbox text-thRed"
+          />
           <span>Remember me</span>
         </div>
         <input
